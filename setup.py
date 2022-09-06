43c4f06ba25bfe6beaf5a9020f0754b36f149d7a
Florentin Labelle <florentin.labelle@student-cs.fr>
2022-09-06 12:02:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup, find_packages
 setup(name='Kubin',
       version='0.2',
       description="Rubik's Cube model and solver",
-      url='http://github.com/Edify-Multitude/Kubin',
-      author='Edify-Multitude',
-      author_email='',
+      url='http://github.com/florentinl/Kubin',
+      author='Florentin Labelle',
+      author_email='florentin.labelle@outlook.fr',
       license='',
       packages=find_packages(exclude=["Kubin"]),
       zip_safe=False)
